b3c764e5113b3e46cc239e8696e094bc16b7bd0e
David Dikman <ddikman@gmail.com>
2021-10-09 18:05:49 +0900

    Added tiptap

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <Example />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import Example from './components/Example.vue';
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld,
+    Example,
   },
 });
 </script>
---
+++ src/components/Example.vue
@@ -0,0 +1,35 @@
+<template>
+  <editor-content :editor="editor" />
+</template>
+
+<script  lang="ts">
+import { defineComponent } from 'vue';
+import { Editor, EditorContent } from '@tiptap/vue-3';
+import StarterKit from '@tiptap/starter-kit';
+
+interface ViewModel {
+  editor?: Editor;
+}
+
+export default defineComponent({
+  components: {
+    EditorContent,
+  },
+  data() {
+    return { editor: undefined } as ViewModel;
+  },
+  mounted() {
+    this.editor = new Editor({
+      content: '<p>I’m running tiptap with Vue.js. 🎉</p>',
+      extensions: [
+        StarterKit,
+      ],
+    });
+  },
+  beforeUnmount() {
+    if (this.editor) {
+      this.editor.destroy();
+    }
+  },
+});
+</script>
